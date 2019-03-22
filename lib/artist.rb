536432e6f5cee63d7655c2a31e3091e594f6a395
Levik Naparstek <levik010@gmail.com>
2019-03-22 19:23:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,33 +1,33 @@
 class Artist
-  attr_accessor :name, :genre, :song
-  @@all = []
+
+  attr_accessor :name, :artist, :genre
+
+  ALL=[]
 
   def initialize(name)
-    @songs = []
     @name = name
-    @@all << self
-  end
-  def new_song(songs, genre)
-    s = Song.new(name, genre)
-    s.songs
+    ALL << self
   end
 
-  def songs=(songs)
-     @songs = songs
-    
-  end
-
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
   def self.all
-    @@all
+    ALL
   end
 
-  def genres                       
-    self.songs.collect do |song|    
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+    self.songs.last
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def genres
+    Song.all.map do |song|
       song.genre
     end
   end
+
 end
